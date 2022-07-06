f77020db12de0ac369e09178f10b8ca73e2dd879
Aniass <a.staskiewicz4@gmail.com>
2022-07-06 16:59:14 +0200

    code refactor

--- clean_data.py
+++ clean_data.py
@@ -7,38 +7,39 @@ from nltk.stem import PorterStemmer
 stop = stopwords.words('english')
 porter = PorterStemmer()
 
+URL_DATA = '\data\products_description.csv'
+
 
 def remove_punctuation(description):
-    """The function to remove punctuation"""
+    """Function to remove punctuation"""
     table = str.maketrans('', '', string.punctuation)
     return description.translate(table)
 
 
 def remove_stopwords(text):
-    """The function to removing stopwords"""
+    """Function to removing stopwords"""
     text = [word.lower() for word in text.split() if word.lower() not in stop]
     return " ".join(text)
 
 
 def stemmer(stem_text):
-    """The function to apply stemming"""
+    """Function to apply stemming"""
     stem_text = [porter.stem(word) for word in stem_text.split()]
     return " ".join(stem_text)
 
 
-# Load dataset
-url = 'C:\\Python Scripts\\API_products\\products_description.csv'
-dataset = pd.read_csv(url, header=0, index_col=0)
+def read_data(path):
+    """Function to read and clean text data"""
+    data = pd.read_csv(path, header=0, index_col=0)
+    data['description'] = data['description'].astype(str)
+    data['description'] = data['description'].apply(remove_punctuation)
+    data['description'] = data['description'].apply(remove_stopwords)
+    data['description'] = data['description'].apply(stemmer)
+    return data
 
-# shape
-print(dataset.shape)
-print(dataset.head(5))
 
-dataset['description'] = dataset['description'].astype(str)
-dataset['description'] = dataset['description'].apply(remove_punctuation)
-dataset['description'] = dataset['description'].apply(remove_stopwords)
-dataset['description'] = dataset['description'].apply(stemmer)
-print(dataset[:5])
-
-data = dataset.to_csv('C:\\Python Scripts\\API_products\\products_clean.csv',
-                      encoding='utf-8')
+if __name__ == '__main__':
+    dataset = read_data(URL_DATA)
+    print(dataset.shape)
+    print(dataset[:5])
+    dataset.to_csv('data\products_clean.csv',encoding='utf-8')
