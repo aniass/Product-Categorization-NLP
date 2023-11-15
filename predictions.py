2f297df27a5e157350844ec25064746403e938f6
Aniass <a.staskiewicz4@gmail.com>
2023-11-15 17:42:06 +0100

    changed preprocess function

--- predictions.py
+++ predictions.py
@@ -3,23 +3,21 @@ from nltk.stem import PorterStemmer
 from joblib import load
 
 
-MODELSPATH = '\models\SVC_model2.pkl'
-
-stop = stopwords.words('english')
-porter = PorterStemmer()
+MODELS_PATH = '\models\SVC_model2.pkl'
 
 
 def load_model():
     '''Loading pretrained model'''
-    with open(MODELSPATH, 'rb') as file:
+    with open(MODELS_PATH, 'rb') as file:
         model = load(file)
         return model
 
 
 def preprocess_data(text):
     ''' Applying stopwords and stemming on raw data'''
-    words = [word.lower() for word in text if word.lower() not in stop]
-    words = [porter.stem(word) for word in words]
+    stop_words = set(stopwords.words('english'))
+    porter = PorterStemmer()
+    words = [porter.stem(word.lower()) for word in text if word.lower() not in stop_words]
     return words
 
 
@@ -27,8 +25,8 @@ def get_prediction(input_text):
     ''' Generating predictions from raw data'''
     model = load_model()
     data = [input_text]
-    text =  preprocess_data(data)
-    prediction = model.predict(text)
+    processed_text =  preprocess_data(data)
+    prediction = model.predict(processed_text)
     result = ''.join(prediction)
     print('---------------')
     print(f'Your product is in category: {result}')
